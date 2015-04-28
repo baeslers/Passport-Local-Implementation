19468614a14e7c16fb9efe8ab6384d0221f2d469
h3xc0ntr0l <h3xc0ntr0l@gmail.com>
2015-04-28 14:02:48 -0400

    yay!

--- routes/admin.js
+++ routes/admin.js
@@ -7,6 +7,7 @@ module.exports = function(app, passport){
   var router = express.Router();
   router.use(function(req, res, next){
     console.log(req.session);
+    next();
   });
   router.route('/')
     .get(function(req, res){
@@ -15,8 +16,8 @@ module.exports = function(app, passport){
 
   router.route('/users')
     .get(function(req, res){
-      User.find({}, function(users){
-        res.json(users);
+      User.find({}, '', function(users){
+        console.log(users);
       });
     })
     .post(function(req, res){
