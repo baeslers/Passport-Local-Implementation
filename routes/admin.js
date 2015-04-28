132215ed847b23f899f468d17aad96c8a18553ef
h3xc0ntr0l <h3xc0ntr0l@gmail.com>
2015-04-28 11:35:26 -0400

    got auth working, now adding client stuff

--- routes/admin.js
+++ routes/admin.js
@@ -13,6 +13,16 @@ module.exports = function(app, passport){
       res.sendFile('admin.html');
     });
 
+  router.route('/users')
+    .get(function(req, res){
+      User.find({}, function(users){
+        res.json(users);
+      });
+    })
+    .post(function(req, res){
+      //create user
+    });
+
   app.use('/admin', router);
 
 };
